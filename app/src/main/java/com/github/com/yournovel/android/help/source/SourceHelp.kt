6eca2c17146c2e653c65f81e121eff505c3e0857
chenjia404 <chenjiablog@gmail.com>
2026-04-11 14:22:33 +0800

    移除书源 18+ 域名黑名单检测及相关资源

--- app/src/main/java/com/github/com/yournovel/android/help/source/SourceHelp.kt
+++ app/src/main/java/com/github/com/yournovel/android/help/source/SourceHelp.kt
@@ -12,24 +12,8 @@ import com.github.com.yournovel.android.help.coroutine.Coroutine
 import com.github.com.yournovel.android.model.AudioPlay
 import com.github.com.yournovel.android.model.ReadBook
 import com.github.com.yournovel.android.model.ReadManga
-import com.github.com.yournovel.android.utils.EncoderUtils
-import com.github.com.yournovel.android.utils.NetworkUtils
-import com.github.com.yournovel.android.utils.splitNotBlank
-import com.github.com.yournovel.android.utils.toastOnUi
-import splitties.init.appCtx
 
 object SourceHelp {
-
-    private val list18Plus by lazy {
-        try {
-            return@lazy String(appCtx.assets.open("18PlusList.txt").readBytes())
-                .splitNotBlank("\n").map {
-                    EncoderUtils.base64Decode(it)
-                }.toHashSet()
-        } catch (_: Exception) {
-            return@lazy emptySet()
-        }
-    }
 
     fun getSource(key: String?): BaseSource? {
         key ?: return null
@@ -117,45 +101,14 @@ object SourceHelp {
     }
 
     fun insertRssSource(vararg rssSources: RssSource) {
-        val rssSourcesGroup = rssSources.groupBy {
-            is18Plus(it.sourceUrl)
-        }
-        rssSourcesGroup[true]?.forEach {
-            appCtx.toastOnUi("${it.sourceName}是18+网址,禁止导入.")
-        }
-        rssSourcesGroup[false]?.let {
-            appDb.rssSourceDao.insert(*it.toTypedArray())
-        }
+        appDb.rssSourceDao.insert(*rssSources)
     }
 
     fun insertBookSource(vararg bookSources: BookSource) {
-        val bookSourcesGroup = bookSources.groupBy {
-            is18Plus(it.bookSourceUrl)
-        }
-        bookSourcesGroup[true]?.forEach {
-            appCtx.toastOnUi("${it.bookSourceName}是18+网址,禁止导入.")
-        }
-        bookSourcesGroup[false]?.let {
-            appDb.bookSourceDao.insert(*it.toTypedArray())
-        }
+        appDb.bookSourceDao.insert(*bookSources)
         Coroutine.async {
             adjustSortNumber()
         }
-    }
-
-    private fun is18Plus(url: String?): Boolean {
-        if (list18Plus.isEmpty()) {
-            return false
-        }
-        url ?: return false
-        val baseUrl = NetworkUtils.getBaseUrl(url) ?: return false
-        kotlin.runCatching {
-            val host = baseUrl.split("//", ".").let {
-                if (it.size > 2) "${it[it.lastIndex - 1]}.${it.last()}" else return false
-            }
-            return list18Plus.contains(host)
-        }
-        return false
     }
 
     /**
